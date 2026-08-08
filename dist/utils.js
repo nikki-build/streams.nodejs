1a3d0087af758580d96ad19defa9d2d6968a15f8
nikki <dev.nikki.build@gmail.com>
2026-08-08 21:07:56 +0530

    code updated

--- dist/utils.js
+++ dist/utils.js
@@ -1,7 +1,37 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeAddress = normalizeAddress;
 exports.stripProtocol = stripProtocol;
@@ -10,7 +40,7 @@ exports.isLocalAddress = isLocalAddress;
 exports.buildWebSocketURL = buildWebSocketURL;
 exports.validateVolume = validateVolume;
 exports.validateSessionID = validateSessionID;
-const node_os_1 = __importDefault(require("node:os"));
+const os = __importStar(require("node:os"));
 /**
  * Ensures the address starts with ws://
  */
@@ -57,7 +87,13 @@ function isLocalAddress(address) {
         return true;
     if (host.startsWith("127."))
         return true;
-    const interfaces = node_os_1.default.networkInterfaces();
+    if (host.startsWith("192."))
+        return true;
+    if (host.startsWith("172."))
+        return true;
+    if (host.startsWith("10."))
+        return true;
+    const interfaces = os.networkInterfaces();
     for (const iface of Object.values(interfaces)) {
         if (!iface)
             continue;
--- examples/basic.ts
+++ examples/basic.ts
@@ -1,3 +1,18 @@
-import { SDK } from "../dist/index.js";
+import { SDK } from "nikki.streams";
 
-SDK.initialize("abc123");
+SDK.initialize("abc123456789");
+
+function devCallback(data:any){
+    console.debug("data ", JSON.stringify(data))
+}
+
+let dev = SDK.createDevice("192.168.1.69:3000", devCallback)
+
+dev.connect()
+
+
+let status = false
+setInterval(() => {
+    status = !status;
+    dev.setLightStatus(status);
+}, 3000);
